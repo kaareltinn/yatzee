c085b66fbe6091e4d82460ad896fb41fa18c4e4f
Kaarel Tinn <kaareltinn@hotmail.com>
2018-06-06 10:13:25 +0300

    Add docs to Game server client functions

--- lib/yatzee/game.ex
+++ lib/yatzee/game.ex
@@ -2,22 +2,60 @@ defmodule Yatzee.Game do
   use GenServer
 
   # CLIENT
+
+  @doc """
+  Creates new game
+  """
   def start_link() do
     GenServer.start_link(__MODULE__, :ok, [])
   end
 
+  @doc """
+  Adds new player to the game
+
+  Returns {result, new_game_state}, where result is one of following:
+  - :ok
+  - :invalid_action
+  """
   def add_player(game, player_name) do
     GenServer.call(game, {:add_player, player_name})
   end
 
+  @doc """
+  Starts the game
+
+  Change game state from :waiting_for_players -> {:throwing_1, player_name}
+
+  Returns {result, new_game_state}, where result is one of following:
+  - :ok
+  - :invalid_action
+  """
   def start_game(game) do
     GenServer.call(game, :start_game)
   end
 
+  @doc """
+  Throws the dices
+
+  Accepts names of the dices which will be thrown again
+
+  Returns {result, new_game_state}, where result is one of following:
+  - :ok
+  - :invalid_action
+  """
   def throw(game, dice_names) do
     GenServer.call(game, {:throw, dice_names})
   end
 
+  @doc """
+  Sets currently choosing players corresponding category field on scorecard
+
+  Returns {result, new_game_state}, where result is one of following:
+  - :ok
+  - :already_set
+  - :no_match
+  - :invalid_action
+  """
   def choose(game, category) do
     GenServer.call(game, {:choose, category})
   end
